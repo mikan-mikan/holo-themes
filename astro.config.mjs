e3f2733c8c0f5a47712c76f33879caee328347c6
mikan-mikan <51882748+mikan-mikan@users.noreply.github.com>
2024-06-23 21:19:11 +0900

    GA4のビルドエラー解消

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 // import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+});
